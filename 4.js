49b50a3494fb66f8eb382264b828aa32b6a164c6
Marshall Bowers <mad.mod.max@gmail.com>
2014-01-13 15:59:30 -0500

    Problem 4 completed successfully.

---
+++ 4.js
@@ -0,0 +1,18 @@
+(function() {
+	function palindrome(n) {
+		return n == n.toString().split("").reverse().join("") ? true : false;
+	};
+
+
+	var largest = 0;
+	
+	for (var i = 100; i < 1000; i++) {
+		for (var j = 100; j < 1000; j++) {
+			if (palindrome(i * j) && i * j > largest) {
+				largest = i * j;
+			}
+		}
+	}
+
+	console.log(largest);
+})();
